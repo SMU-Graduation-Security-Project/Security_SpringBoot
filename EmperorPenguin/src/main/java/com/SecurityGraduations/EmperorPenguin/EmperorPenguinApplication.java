638133739424d4b6ad5075bf0bd5668411e8d3f3
arad4228 <arad4228@gmail.com>
2022-03-14 11:34:48 +0900

    Add ignore File and EmperoPenguin Spring

---
+++ EmperorPenguin/src/main/java/com/SecurityGraduations/EmperorPenguin/EmperorPenguinApplication.java
@@ -0,0 +1,13 @@
+package com.SecurityGraduations.EmperorPenguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmperorPenguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmperorPenguinApplication.class, args);
+	}
+
+}
