38066516455c80f92321687b2d5f79dd8945c022
arad4228 <arad4228@gmail.com>
2022-03-23 17:50:50 +0900

    Delete Spring Exception's

--- EmperorPenguin/src/main/java/com/SecurityGraduations/EmperorPenguin/exception/IdExistedException.java
+++
@@ -1,7 +0,0 @@
-package com.SecurityGraduations.EmperorPenguin.exception;
-
-public class IdExistedException extends RuntimeException {
-    public IdExistedException(String id) {
-        super(id+"는 이미 존재합니다.");
-    }
-}
--- EmperorPenguin/src/main/java/com/SecurityGraduations/EmperorPenguin/exception/PasswordWrongException.java
+++
@@ -1,7 +0,0 @@
-package com.SecurityGraduations.EmperorPenguin.exception;
-
-public class PasswordWrongException extends RuntimeException {
-    public PasswordWrongException() {
-        super("Password가 옳지 않습니다.");
-    }
-}
