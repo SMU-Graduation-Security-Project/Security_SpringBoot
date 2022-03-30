cb02ffb596a40868388b100cee44e0e87e206a68
arad4228 <arad4228@gmail.com>
2022-03-30 10:37:48 +0900

    Fix Server Time and Testing Cert

--- EmperorPenguin/src/main/java/com/SecurityGraduations/EmperorPenguin/EmperorPenguinApplication.java
+++ EmperorPenguin/src/main/java/com/SecurityGraduations/EmperorPenguin/EmperorPenguinApplication.java
@@ -3,8 +3,16 @@ package com.SecurityGraduations.EmperorPenguin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EmperorPenguinApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmperorPenguinApplication.class, args);
