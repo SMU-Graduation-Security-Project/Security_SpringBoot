e1f7aa9ca70e8b7c5f75333f1b75c5bad6e9f408
arad4228 <arad4228@gmail.com>
2022-04-05 11:12:48 +0900

    Add New SangmyungBank Project

---
+++ SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/SangmyungBankApplication.java
@@ -0,0 +1,13 @@
+package com.EmperorPenguin.SangmyungBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangmyungBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangmyungBankApplication.class, args);
+	}
+
+}
---
+++ SangmyungBank/src/test/java/com/EmperorPenguin/SangmyungBank/SangmyungBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmperorPenguin.SangmyungBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangmyungBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
