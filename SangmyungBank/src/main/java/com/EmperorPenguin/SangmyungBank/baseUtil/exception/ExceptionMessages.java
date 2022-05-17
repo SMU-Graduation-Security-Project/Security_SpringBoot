d9ec2f3173a37a704868db94356923f39454c0ea
arad4228 <arad4228@gmail.com>
2022-05-17 21:19:05 +0900

    Update User Exceptions

--- SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/baseUtil/exception/ExceptionMessages.java
+++ SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/baseUtil/exception/ExceptionMessages.java
@@ -10,8 +10,9 @@ public enum ExceptionMessages {
     ERROR_USER_ID_DUPLICATE("이미 사용중인 아이디 입니다."),
     ERROR_USER_EMAIL_DUPLICATE("이미 사용중인 이메일입니다."),
     ERROR_USER_PHONENUMBER_DUPLICATE("이미 사용중인 전화번호입니다."),
-    ERROR_USER_PASSWORD_FORMAT("비밀번호 형식을 맞춰주세요"),
-    ERROR_USER_PASSWORD("사용자의 비밀번호가 일치하지 않습니다.");
-
+    ERROR_USER_ID_FORMAT("아이디는 영문자로 시작하고 영문자,숫자로 5~12자 이하로 구성되어야합니다."),
+    ERROR_USER_PASSWORD_FORMAT("비밀번호는 영문자, 특수문자를 포함한 8~20자로 구성되어야 합니다."),
+    ERROR_USER_PASSWORD("사용자의 비밀번호가 일치하지 않습니다."),
+    ERROR_USER_NOT_FOUND("사용자를 찾을 수 없습니다.");
     private final String message;
 }
