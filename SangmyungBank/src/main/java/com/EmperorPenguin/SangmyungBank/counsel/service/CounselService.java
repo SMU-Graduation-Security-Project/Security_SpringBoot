4af8eaf8f77ea68dd14571cd46f306bf975c56de
arad4228 <arad4228@gmail.com>
2022-06-28 13:47:45 +0900

    Fix Counsel return Reversed

--- SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/counsel/service/CounselService.java
+++ SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/counsel/service/CounselService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -45,6 +46,7 @@ public class CounselService {
 
         return counselRepository.findAllByMemberId(member)
                 .stream()
+                .sorted(Comparator.comparing(Counsel::getId).reversed())
                 .map(Counsel::toDto)
                 .collect(Collectors.toList());
     }
