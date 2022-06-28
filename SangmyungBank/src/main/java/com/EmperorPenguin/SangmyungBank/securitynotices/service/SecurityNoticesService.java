be5eab647f76904c37a601bbd1929e7f164f72d5
arad4228 <arad4228@gmail.com>
2022-06-28 13:44:57 +0900

    Fix Security Notices return Revers

--- SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/securitynotices/service/SecurityNoticesService.java
+++ SangmyungBank/src/main/java/com/EmperorPenguin/SangmyungBank/securitynotices/service/SecurityNoticesService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +38,7 @@ public class SecurityNoticesService {
     public List<SecurityNoticeInquiryRes> getAllSecurityNotices() {
         return securityNoticesRepository.findAll()
                 .stream()
+                .sorted(Comparator.comparing(SecurityNotices::getId).reversed())
                 .map(SecurityNotices::toDto)
                 .collect(Collectors.toList());
     }
